71191398bd4e73a34876e766a638b348f39bb3d2
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-11-09 00:33:22 +0100

    modif for reverse proxy supporting

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+app.set("trust proxy", 1);
+
 //methode transport mail
 let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
